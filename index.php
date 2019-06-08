801508bd912e16b1e4ce30ca692f0fa3de2baead
Abilash George Thomas <abilash_thomas@apple.com>
2019-06-08 11:32:42 -0700

    moving back to heoroku

--- index.php
+++ index.php
@@ -1,3 +1,128 @@
-<?php
-echo "site under maintenance";
-?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Profile Website of Abilash George Thomas">
+    <meta name="author" content="Abilash George Thomas">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Cover Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="cover.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="style.css" rel="stylesheet">
+  </head>
+
+  <body class="text-center">
+
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+      <header class="masthead mb-auto">
+        <div class="inner">
+          <h3 class="masthead-brand">Abilash George Thomas</h3>
+          <nav class="nav nav-masthead justify-content-center">
+            <a class="nav-link active" href="http://abilashthomas.com">Resume</a>
+            <a class="nav-link" target="_blank" href="https://www.linkedin.com/in/abilashgt">LinkedIn</a>
+          </nav>
+        </div>
+      </header>
+
+      <main role="main" class="inner cover">
+        <h1 class="cover-heading" style="margin: 1em 0">Software Engineer</h1>      
+
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+              <ul class="list-unstyled">
+                <li>I am a Software Engineer with 8 years of experience in developing software solutions.</li>
+                <li>I have acquired skills in creating Microservices, RESTful APIs and web applications using technologies like Java, Spring Framework etc.</li>
+                <li>I have experience analyzing Big Data using Hadoop and Spark.</li>
+                <li>I have developed software infrastructures in various Cloud Platforms.</li>
+                <li>I worked with the IOT Platforms and created end to end software solutions in Cloud and Big Data.</li>
+                <li>I have also gained experience coordinating and leading cross-functional and multi-cultural development teams from onsite and remotely.</li>
+              </ul>  
+            </div>
+          </div>
+          
+          <div class="row" style="margin-top: 2em">
+            <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7 text-align-left">
+              <h5>Education</h5>
+              <p>
+                <b>Mar Athanasius College of Engineering</b>, Kerala, India — Bachelor Degree <br />
+                2006 - 2010 <br /> 
+                Bachelor of Technology in Computer Science and Engineering, Mahatma Gandhi University. 
+              </p>
+              <p>
+                <b>Sri Atmananda Memorial School</b>, Kerala, India — School 1992 - 2006 
+                Grade 10 in ICSE. Grade 12 in ISC
+              </p>
+            </div>
+            <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5 text-align-left">
+              <h5>Personal</h5>
+              <p> 
+                <b>Date of Birth:</b> March 25, 1988 <br />
+                <b>Address:</b> 232 Countrybrook Loop, San Ramon, CA 94583 <br />
+                <b>Country:</b> USA <br />
+                <b>US Visa Type:</b> H1B <br />
+                <b>Citizenship:</b> India 
+              </p>
+            </div>
+          </div>
+
+          <div class="row" style="margin-top: 2em">
+            <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7 text-align-left">
+              <h5>Experience</h5>
+              <p>
+                Tata Consultancy Service Ltd, India & USA — I.T Analyst <br />
+                JAN 2015 - PRESENT   
+              </p>
+              <p>
+                HIFX Learning Service Pvt Ltd, Kochi, India — Senior Software Engineer <br />
+                DEC 2011 - NOV 2014 
+              </p>
+              <p>
+                Matekk Consultancy Services Pvt Ltd, Kochi, India — Developer <br />
+                DEC 2010 - NOV 2011 
+              </p>
+            </div>
+            <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5 text-align-left">
+              <h5>Skills</h5>
+              <p> 
+                <b>Domains:</b> Social Media, Insurance, Retail Industry, Manufacturing <br />
+                <b>Programming Languages:</b> Java, Scala, Python, HTML, JavaScript, PHP <br />
+                <b>Big Data:</b> Spark, Hadoop, Hive <br />
+                <b>Web:</b> Spring, Hibernate, RESTful API’s <br />
+                <b>Databases:</b>  HBase, MongoDB, MySQL, Oracle DB <br />
+                <b>Cloud:</b> Microsoft Azure, Amazon AWS <br />
+                <b>Others:</b> Docker, MQTT 
+              </p>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <footer class="mastfoot mt-auto">
+        <div class="inner">
+          <p>Powered by <a href="https://getbootstrap.com/">Bootstrap</a></p>
+        </div>
+      </footer>
+    </div>
+
+
+    
+    
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+  </body>
+</html>
